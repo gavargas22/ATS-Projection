76e35f40830d3e8bba229d40eba9d37949cdc439
Guillermo Vargas <gavargas22@gmail.com>
2014-07-29 18:39:08 -0600

    Added some other stuff
    I am trying to make the random selector not repeat the items that are
    already chosen.

--- js/init.js
+++ js/init.js
@@ -1,15 +1,20 @@
-// use document ready event to make sure elements exists
-        $("#video").html(function(){
-            var videoArray = [
-                'Ig-DbfPoz3o',
-                'estPhyfBGho',
-                '6JL4hpnZklk',
-                'WpXfQMFR_Qs',
-                'JtxQT7aVDeg',
-                'eSCvCAC7Q-c',
+$(".videoWrapper").html(function(){
 
-            ];
-            var index=Math.floor(Math.random() * videoArray.length);
-            var html='<iframe width="840px" height="480px" src="http://www.youtube.com/embed/' + videoArray[index] + '" frameborder="0" allowfullscreen></iframe>';
-            return html;
-        });
+    function removeItemFromArray() {
+        videoArray.splice(index, 1);
+    }
+
+    var videoArray = [
+    'Ig-DbfPoz3o',
+    'estPhyfBGho',
+    '6JL4hpnZklk',
+    'WpXfQMFR_Qs',
+    'JtxQT7aVDeg',
+    'eSCvCAC7Q-c',
+    ];
+    var index=Math.floor(Math.random() * videoArray.length);
+    var html='<iframe width="840px" height="480px" src="http://www.youtube.com/embed/' + videoArray[index] + '" frameborder="0" allowfullscreen></iframe>' + document.write(index);
+    removeItemFromArray();
+    return html;
+}
+);
